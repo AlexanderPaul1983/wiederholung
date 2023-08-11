336a45ca9f8726b571cfb3ff1e4d2cd15ab710df
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-08-11 12:11:59 +0200

    solution to the Task: Multiply two intergers recursive.

---
+++ src/Aufgabe_2/Multiplikation.java
@@ -0,0 +1,29 @@
+package Aufgabe_2;
+
+public class Multiplikation {
+    public int multiRecursive(int a, int b){
+        int sum = 0;
+        if(b == 0 || a == 0){
+            return 0;
+
+        } else if (a == 1) {
+            return b;
+
+        } else if (b == 1) {
+            return a;
+
+        } else if (b < 0) {
+            sum = a + multiRecursive(a,b+1);
+            return sum;
+
+        } else {
+            sum = a + multiRecursive(a,b-1);
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Multiplikation multi = new Multiplikation();
+        System.out.println(multi.multiRecursive(3,-5));
+    }
+}
