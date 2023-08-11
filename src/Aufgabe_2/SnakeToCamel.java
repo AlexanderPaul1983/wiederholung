d9c42abfea233cdc331d0b9a5a6fff6c27bdb0cd
AlexanderPaul1983 <alexanderpaul@gmx.de>
2023-08-11 15:30:06 +0200

    Snake to Camel case task was created

---
+++ src/Aufgabe_2/SnakeToCamel.java
@@ -0,0 +1,26 @@
+package Aufgabe_2;
+
+import java.util.Arrays;
+
+public class SnakeToCamel {
+    public String changeFromSnakeToCamel(String snakeString){
+        String[] dividedWords = snakeString.split("_");
+        // wenn der String nur aus einem Word besteht, wird er ausgegeben
+        if(dividedWords.length == 1){
+            return Arrays.toString(dividedWords);
+        }
+        // In einem anderen Fall müssen alle Wörter im Array großgeschrieben werden und dann zusammen gefügt.
+        for (int i = 1; i < dividedWords.length ; i++) {
+            dividedWords[i] = dividedWords[i].substring(0, 1).toUpperCase() + dividedWords[i].substring(1);
+
+
+        }
+        return String.join("", dividedWords);
+
+    }
+
+    public static void main(String[] args) {
+        SnakeToCamel s = new SnakeToCamel();
+        System.out.println(s.changeFromSnakeToCamel("hallo"));
+    }
+}
